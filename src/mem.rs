e52302882a1ea7bda3e0aa2351484a70409e5810
provrb <ethanjamesoliveira@gmail.com>
2025-02-21 04:37:16 +0000

    start adding linked list allocator

--- src/mem.rs
+++ src/mem.rs
@@ -1,5 +1,5 @@
 use bootloader::bootinfo::MemoryMap;
-use linked_list_allocator::LockedHeap;
+use linked_list_allocator::{align_up, LockedHeap};
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, OffsetPageTable, Page, PageTable,
@@ -41,6 +41,55 @@ pub fn heap_init(
     }
 
     Ok(())
+}
+
+struct ListNode {
+    size: usize,
+    next:Option<&'static mut ListNode>,
+}
+
+impl ListNode {
+    const fn new(size: usize) -> Self {
+        Self {
+            size,
+            next: None,
+        }
+    }
+
+    fn start_addr(&self) -> usize {
+        self as *const Self as usize
+    }
+
+    fn end_addr(&self) -> usize {
+        self.start_addr() + self.size
+    }
+}
+
+pub struct LinkedAllocator {
+    head: ListNode,
+}
+
+impl LinkedAllocator {
+    pub const fn new() -> Self {
+        Self {
+            head: ListNode::new(0),
+        }
+    }
+
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.add_free_region(heap_start, heap_size);
+    }
+
+    pub unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        assert_eq!(align_up(addr, align_of::<ListNode>()), addr); // freed region can hold ListNode
+        assert!(size >= size_of::<ListNode>());
+
+        let mut node = ListNode::new(size);
+        node.next = self.head.next.take();
+        let node_ptr = addr as *mut ListNode;
+        node_ptr.write(node);
+        self.head.next = Some(&mut *node_ptr);
+    }
 }
 
 unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
